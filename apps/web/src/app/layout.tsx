3fed9a3d59c6b9d062085befa14f79c07c855920
JuliusCordova <julio.cordova@gmail.com>
2026-08-31 09:11:24 -0500

    Feature 59 · Mount AgentOps navigation sync

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import AgentGovernanceEnhancer from "./components/AgentGovernanceEnhancer";
 import AgentGovernanceEvidenceSections from "./components/AgentGovernanceEvidenceSections";
+import AgentGovernanceNavigationSync from "./components/AgentGovernanceNavigationSync";
 import DemoControls from "./components/DemoControls";
 import ExecutiveDemoRibbon from "./components/ExecutiveDemoRibbon";
 import ProductNavigation from "./components/ProductNavigation";
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <SessionBanner />
         <ProductNavigation />
         {children}
+        <AgentGovernanceNavigationSync />
         <AgentGovernanceEnhancer />
         <AgentGovernanceEvidenceSections />
         <DemoControls />
